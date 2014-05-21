bc93ff80e17cf9c1b1d96caf6bf7bf3fc905970f
Hirokazu Odaka <odaka@astro.isas.jaxa.jp>
2014-05-21 10:02:10 +0900

    Add schema for HXI EIC/MIC.

---
+++ clients/hsql_client/user_data/schema/HXI_EICMIC/HXI1.CAMERA.IMAGES.js
@@ -0,0 +1,47 @@
+HSQuickLook.main.schema =
+[
+{
+ "collection" : "hxi1",
+ "functionalObject": "/HXI1.QL",
+ "attributeSequence": "Camera",
+ "period" : 2,
+ "blockName" : "Block_image",
+ "contents" : {
+   "image_0" : {"type": "image"},
+   "image_1" : {"type": "image"},
+   "image_2" : {"type": "image"},
+   "image_3" : {"type": "image"},
+   "image_4" : {"type": "image"}
+ }
+},
+{
+ "collection" : "hxi1",
+ "functionalObject": "/HXI1.QL",
+ "attributeSequence": "Camera",
+ "period" : 2,
+ "blockName" : "Block_hist_all",
+ "contents" : {
+   "hist_all" : {"type": "image"},
+   "hist_all_cmn" : {"type": "image"}
+ }
+},
+{
+ "collection" : "hxi1",
+ "functionalObject": "/HXI1.QL",
+ "attributeSequence": "Camera",
+ "period" : 2,
+ "blockName" : "Block_hist_asic",
+ "contents" : {
+   "hist_asic_cmn_00" : {"type": "image"},
+   "hist_asic_cmn_04" : {"type": "image"},
+   "hist_asic_cmn_08" : {"type": "image"},
+   "hist_asic_cmn_12" : {"type": "image"},
+   "hist_asic_cmn_16" : {"type": "image"},
+   "hist_asic_cmn_20" : {"type": "image"},
+   "hist_asic_cmn_24" : {"type": "image"},
+   "hist_asic_cmn_28" : {"type": "image"},
+   "hist_asic_cmn_32" : {"type": "image"},
+   "hist_asic_cmn_36" : {"type": "image"}
+ }
+}
+];
--- clients/hsql_client/user_data/schema/replace.rb
+++
@@ -1,8 +0,0 @@
-Dir::glob("*.js") do |file|
-  s = nil
-  File.open(file){|fin|
-      s = fin.read
-      s.sub!("hk1", "hxi1")
-      fin.write(s)
-  }
-end
