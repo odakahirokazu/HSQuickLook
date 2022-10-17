ab4baa53b86ce07a5267a40756be2ee969e55f1d
Hirokazu Odaka <hirokazu.odaka@phys.s.u-tokyo.ac.jp>
2022-10-17 13:22:57 +0900

    MongoDBClient: development from cipher SPring-8 2021B; - add methods related to new quick look by Taihei Watanabe - selectable instantiation - add a port paremeter

--- analyzer/source/include/MongoDBClient.hh
+++ analyzer/source/include/MongoDBClient.hh
@@ -4,6 +4,8 @@
  * @author Hirokazu Odaka
  * @date 2013-01-06
  * @date 2019-10-09 | version 2.0
+ * @date 2021-11-02 | version 3.0 | mongo instantiation selectable
+ * @date 2022-10-07 | version 3.1 | add a parameter port
  *
  */
 
@@ -22,27 +24,35 @@ namespace hsquicklook {
 
 class MongoDBClient : public anlnext::BasicModule
 {
-  DEFINE_ANL_MODULE(MongoDBClient, 2.0);
+  DEFINE_ANL_MODULE(MongoDBClient, 3.1);
 public:
   MongoDBClient();
 
   anlnext::ANLStatus mod_define() override;
   anlnext::ANLStatus mod_initialize() override;
+  anlnext::ANLStatus mod_finalize() override;
 
   void createCappedCollection(const std::string& name, int size);
   void createCollection(const std::string& name);
   void push(const std::string& collection, const bsoncxx::document::value& doc);
   void push_many(const std::string& collection, std::vector<bsoncxx::document::value>& docs);
+  bsoncxx::stdx::optional<bsoncxx::document::value> find_one(const std::string& collection, const bsoncxx::document::value& constraints);
+  mongocxx::cursor find(const std::string& collection, const bsoncxx::document::value& constraints);
+  mongocxx::cursor aggregate(const std::string& collection, const mongocxx::pipeline& pipeline);
+  void update(const std::string& collection, const bsoncxx::document::value& constraints, bsoncxx::document::value& doc);
+  void update_many(const std::string& collection, const bsoncxx::document::value& constraints, bsoncxx::document::value& doc, bsoncxx::array::value& array_filters);
 
   mongocxx::database& getDatabase()
   { return m_DB; }
   
 private:
-  mongocxx::instance m_MDBInstance;
+  std::unique_ptr<mongocxx::instance> m_MDBInstance;
   std::unique_ptr<mongocxx::client> m_Client;
   mongocxx::database m_DB;
   std::string m_MDBHost;
+  int m_MDBPort;
   std::string m_MDBName;
+  bool m_MDBInstantiation = true;
 };
 
 } /* namespace hsquicklook */
--- analyzer/source/src/MongoDBClient.cc
+++ analyzer/source/src/MongoDBClient.cc
@@ -8,23 +8,40 @@ namespace hsquicklook
 {
 
 MongoDBClient::MongoDBClient()
-  : m_MDBHost("localhost"), m_MDBName("hsquicklook")
+  : m_MDBHost("localhost"),
+    m_MDBPort(27017),
+    m_MDBName("hsquicklook"),
+    m_MDBInstantiation(true)
 {
 }
 
 ANLStatus MongoDBClient::mod_define()
 {
   define_parameter("host", &mod_class::m_MDBHost);
+  define_parameter("port", &mod_class::m_MDBPort);
   define_parameter("database", &mod_class::m_MDBName);
+  define_parameter("instantiation", &mod_class::m_MDBInstantiation);
 
   return AS_OK;
 }
 
 ANLStatus MongoDBClient::mod_initialize()
 {
-  mongocxx::uri uri((boost::format("mongodb://%s:27017")%m_MDBHost).str());
+  if (m_MDBInstantiation) {
+    m_MDBInstance = std::make_unique<mongocxx::instance>();
+  }
+  mongocxx::uri uri((boost::format("mongodb://%s:%d") % m_MDBHost % m_MDBPort).str());
   m_Client.reset(new mongocxx::client(uri));
   m_DB = m_Client->database(m_MDBName);
+
+  return AS_OK;
+}
+
+ANLStatus MongoDBClient::mod_finalize()
+{
+  if (m_MDBInstantiation) {
+    m_MDBInstance.reset();
+  }
 
   return AS_OK;
 }
@@ -68,6 +85,38 @@ void MongoDBClient::push_many(const std::string& collection, std::vector<bsoncxx
   if (docs.size() > 0) {
     col.insert_many(docs);
   }
+}
+
+bsoncxx::stdx::optional<bsoncxx::document::value> MongoDBClient::find_one(const std::string& collection, const bsoncxx::document::value& constraints)
+{
+  mongocxx::collection col = m_DB[collection];
+  return col.find_one(constraints.view());
+}
+
+mongocxx::cursor MongoDBClient::find(const std::string& collection, const bsoncxx::document::value& constraints)
+{
+  mongocxx::collection col = m_DB[collection];
+  return col.find(constraints.view());
+}
+
+mongocxx::cursor MongoDBClient::aggregate(const std::string& collection, const mongocxx::pipeline& pipeline)
+{
+  mongocxx::collection col = m_DB[collection];
+  return col.aggregate(pipeline, mongocxx::options::aggregate{});
+}
+
+void MongoDBClient::update(const std::string& collection, const bsoncxx::document::value& constraints, bsoncxx::document::value& doc)
+{
+  mongocxx::collection col = m_DB[collection];
+  col.update_one(constraints.view(), doc.view());
+}
+
+void MongoDBClient::update_many(const std::string& collection, const bsoncxx::document::value& constraints, bsoncxx::document::value& doc, bsoncxx::array::value& array_filters)
+{
+  mongocxx::collection col = m_DB[collection];
+  mongocxx::options::update options;
+  options.array_filters(array_filters.view());
+  col.update_many(constraints.view(), doc.view(), options);
 }
 
 } /* namespace hsquicklook */
